2eeae9adcdf1f0ee792ebe5e9bddadb4b6a35703
pro skidder <133696170+packersonmakepackerson@users.noreply.github.com>
2024-07-16 14:33:05 +0200

    Create Installer.lua

---
+++ Installer.lua
@@ -0,0 +1,6 @@
+local github = https://raw.githubusercontent.com/packersonmakepackerson/Ignited-open-src/main/Ignited%200.4.0
+local function ignitedGitHubRequest
+    if isfile("Ignited") == false then
+        lplr:Kick("Could Not Connect to Github!")
+if isfile("Ignited") == true then
+    loadstring(GameHttpGet((https://raw.githubusercontent.com/packersonmakepackerson/Ignited-open-src/main/Ignited%200.4.0),))()
